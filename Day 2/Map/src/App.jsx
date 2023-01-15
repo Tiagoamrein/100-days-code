696784c8116bb8567cd6474d6fcac1c3b9f207eb
tiago <tiagoamrein@gmail.com>
2023-01-15 14:39:02 -0300

    Metodo .map para manipular arrays

---
+++ Day 2/Map/src/App.jsx
@@ -0,0 +1,34 @@
+
+import './App.css'
+
+function App() {
+  const users = [
+    {
+      name: 'Carlos',
+      address: 'Rua X',
+      age: 28,
+      isAdmin: false,
+    },
+    {
+      name: 'Maria',
+      address: 'Rua XX',
+      age: 31,
+      isAdmin: true,
+    },
+    {
+      name: 'Matheus',
+      address: 'Rua XI',
+      age: 22,
+      isAdmin: false,
+    }
+  ]
+
+  return (
+   <div>
+    {users.map((user) => ( <div> {user.name}, {user.age} </div>))}
+
+   </div>
+  )
+}
+
+export default App
